d56afea090ac90aa77f31e1ebddb9714134f2aa2
Bryan Kegley <hi@bryankegley.me>
2019-02-08 13:42:33 -0600

    testing gh env variables

--- .github/log/index.js
+++ .github/log/index.js
@@ -1,1 +1,28 @@
-console.log("Action running!");
+const fs = require("fs");
+
+const workflow = process.env.GITHUB_WORKFLOW;
+const action = process.env.GITHUB_ACTION;
+const actor = process.env.GITHUB_ACTOR;
+const repo = process.env.GITHUB_REPOSITORY;
+const eventName = process.env.GITHUB_EVENT_NAME;
+const eventPath = process.env.GITHUB_EVENT_PATH;
+const workspace = process.env.GITHUB_WORKSPACE;
+const sha = process.env.GITHUB_SHA;
+const ref = process.env.GITHUB_REF;
+const token = process.env.GITHUB_TOKEN;
+
+console.log({
+  workflow,
+  action,
+  actor,
+  repo,
+  eventName,
+  eventPath,
+  workspace,
+  sha,
+  ref,
+  token
+});
+
+const payload = fs.readFileSync(eventPath);
+console.log({ payload });
